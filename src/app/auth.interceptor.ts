686f47cbbd44e95a5460863a753638e873d32181
Moisés Almeida <moises@debian.mba.org>
2021-05-07 15:54:08 -0300

    refreshToken

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpEvent, HttpResponse, HttpRequest, HttpHandler } from '@angular/common/http';
+import { HttpInterceptor, HttpEvent, HttpResponse, HttpRequest, HttpHandler, HttpErrorResponse } from '@angular/common/http';
 import { Observable, EMPTY, throwError, BehaviorSubject } from 'rxjs';
 import { OAuth2Response } from './oauth2'
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { LoginService } from './login/login.service';
 import { catchError, delay, filter, finalize, switchMap, take } from 'rxjs/operators';
 import { Subject } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 
 
@@ -43,64 +44,94 @@ intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<an
       console.log(err);
 
       //if (err instanceof HttpErrorResponse && err.status === 401) 
+
       if (err.status == 401) {
 
-        //if (err.error.message.indexOf('expired') > -1 ) 
-
-        if(!this.isRefreshingToken) {
-           this.isRefreshingToken = true;
-           this.refreshAccessTokenSubject.next(null);
-
-           return this.loginService.refreshToken().pipe(
-                    switchMap(res =>{
-                      console.log(res);
-                      if(res.access_token) {
-                        this.loginService.applyToken(res);
-                        this.refreshAccessTokenSubject.next(res.access_token)
-                        return next.handle(request)
-                      } else {
-                        alert("retornou sucesso porem sem o token");
-                        // this.loginService.gotoLoginPage();
-                        return EMPTY;
-                      }                            
-                    }),
-                    catchError(err =>{
-                      console.log(err); 
-                      //this.loginService.gotoLoginPage();
-                      return EMPTY;
-                    }),
-                    finalize(()=>{ 
-                      this.isRefreshingToken = false;
-                    })
-              );
-          
-          
-        } else {
-          return this.refreshAccessTokenSubject.pipe(
-            filter(token => token != null),
-            take(1),
-            switchMap(jwt => {
-              return next.handle(this.addToken(request, jwt));
-            }));
-        }
-            
+        return this.handleError401(request, next, err);
 
       } if(err.status == 400) { 
-         
-          if (err.error.message.indexOf('not active') > -1 ) {   
-            this.loginService.gotoLoginPage();
-          } 
+
+        return this.handleError400(request, next, err);
 
       } else {
          console.log(err);
+         alert(err);
       } 
       return EMPTY;
   }) //FIM catchError
  );
 
-
 }
 
+private handleError401(request: HttpRequest<any>, next: HttpHandler, error: Error): Observable<HttpEvent<any>> {
+  //if (err.error.message.indexOf('expired') > -1 ) 
+
+  if(!this.isRefreshingToken) {
+    this.isRefreshingToken = true;
+    this.refreshAccessTokenSubject.next(null);
+
+    return this.loginService.refreshToken().pipe(
+            switchMap(res =>{
+              console.log(res);
+              if(res.access_token) {
+                this.loginService.applyToken(res);
+                this.refreshAccessTokenSubject.next(res.access_token)
+                return next.handle(request)
+              } else {
+                alert("retornou sucesso porem sem o token");
+                // this.loginService.gotoLoginPage();
+                return EMPTY;
+              }                            
+            }),
+            catchError(err =>{
+              console.log(err); 
+              //this.loginService.gotoLoginPage();
+              return EMPTY;
+            }),
+            finalize(()=>{ 
+              this.isRefreshingToken = false;
+            })
+      );
+  
+  
+  } else {
+  return this.refreshAccessTokenSubject.pipe(
+    filter(token => token != null),
+    take(1),
+    switchMap(jwt => {
+      return next.handle(this.addToken(request, jwt));
+    }));
+  }
+    
+}
+
+
+private handleError400(request: HttpRequest<any>, next: HttpHandler, err: any): Observable<HttpEvent<any>> {
+
+  let isCallToAuthenticationServer = (request.url.indexOf(environment.keycloakAuthToken) > -1);
+          
+  if (isCallToAuthenticationServer) {   
+
+      let erroMessage = err?.error?.message; 
+      if(erroMessage == null) {
+        erroMessage = err?.error?.error_description; 
+      }
+
+      if(erroMessage && erroMessage.toLowerCase().indexOf("invalid refresh token") != -1){
+        alert("Sessão expirada.");
+        this.loginService.gotoLoginPage();
+      } else {
+        alert(err)
+      }
+   
+  } else {
+    alert(err);
+  } 
+  
+  
+
+  return EMPTY;
+}
 
  
 
